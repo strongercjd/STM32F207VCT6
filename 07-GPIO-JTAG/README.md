5613c1dd3cbd17261f74748e261caf6663ed4f6d
strongercjd <strongercjd@163.com>
2020-07-12 22:23:05 +0800

    上传GPIO-JTAG代码

---
+++ 07-GPIO-JTAG/README.md
@@ -0,0 +1,3 @@
+# GPIO-JTAG
+
+上电前，按住KEY1，锁定JTAG，上电时不按KEY1，不会锁定JTAG
---
+++ 07-GPIO-JTAG/main.c
@@ -0,0 +1,183 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F2xx_StdPeriph_Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    13-April-2012
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "LCD_disp.h"
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t TimingDelay;
+
+
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  RCC_ClocksTypeDef RCC_Clocks;
+  GPIO_InitTypeDef  GPIO_Init_s;
+  
+  /* 配置SysTick为1ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
+  
+  /* 使能GPIOE端口时钟 */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+  
+  /* 配置LED管脚 */
+  GPIO_Init_s.GPIO_Pin = GPIO_Pin_4;
+  GPIO_Init_s.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_Init_s.GPIO_OType = GPIO_OType_PP;
+  GPIO_Init_s.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init_s.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOE, &GPIO_Init_s);
+  
+  /*KEY1:PE14 */
+  GPIO_Init_s.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_Init_s.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init_s.GPIO_Pin = GPIO_Pin_14;
+  GPIO_Init(GPIOE, &GPIO_Init_s);
+
+  LCD_Init();
+  
+  /*KEY1*/
+  if(GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_14)){
+    Set_Pen_Color(LCD_grenn);
+    LCD_ShowString(0,0,"JTAG is Unlock");
+  }else{
+    
+    Set_Pen_Color(LCD_red);
+    LCD_ShowString(0,0,"JTAG is Lock");
+    
+    /* Enable GPIOA and GPIOB clocks */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB , ENABLE);
+    
+    /* Configure PA.13 (JTMS/SWDIO), PA.14 (JTCK/SWCLK) and PA.15 (JTDI) as 
+    output push-pull */
+    GPIO_Init_s.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_Init_s.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_Init_s.GPIO_OType = GPIO_OType_PP;
+    GPIO_Init_s.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init_s.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_Init_s);
+    
+    /* Configure PB.03 (JTDO) and PB.04 (JTRST) as output push-pull */
+    GPIO_Init_s.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4;
+    GPIO_Init(GPIOB, &GPIO_Init_s);
+  }
+
+  while (1)
+  {
+    GPIO_SetBits(GPIOE,GPIO_Pin_4);  //熄灭LED灯
+    Delay(500);                      //延时500ms
+    GPIO_ResetBits(GPIOE,GPIO_Pin_4);//点亮LED灯
+    Delay(500);                      //延时500ms
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in 10 ms.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{
+  TimingDelay = nTime;
+
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+
+/*120Mhz时钟时，当ulCount为1时，函数耗时3个时钟，延时=3*1/120us=1/40us*/
+/*
+SystemCoreClock=120000000
+
+us级延时,延时n微秒
+SysCtlDelay(n*(SystemCoreClock/3000000));
+
+ms级延时,延时n毫秒
+SysCtlDelay(n*(SystemCoreClock/3000));
+
+m级延时,延时n秒
+SysCtlDelay(n*(SystemCoreClock/3));
+*/
+
+#if defined   (__CC_ARM) /*!< ARM Compiler */
+__asm void
+SysCtlDelay(unsigned long ulCount)
+{
+    subs    r0, #1;
+    bne     SysCtlDelay;
+    bx      lr;
+}
+#elif defined ( __ICCARM__ ) /*!< IAR Compiler */
+void
+SysCtlDelay(unsigned long ulCount)
+{
+    __asm("    subs    r0, #1\n"
+       "    bne.n   SysCtlDelay\n"
+       "    bx      lr");
+}
+
+#elif defined (__GNUC__) /*!< GNU Compiler */
+void __attribute__((naked))
+SysCtlDelay(unsigned long ulCount)
+{
+    __asm("    subs    r0, #1\n"
+       "    bne     SysCtlDelay\n"
+       "    bx      lr");
+}
+
+#elif defined  (__TASKING__) /*!< TASKING Compiler */                           
+/*无*/
+#endif /* __CC_ARM */
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
