7a6d62a4a84d0fb55634c521d8772e694e020c01
strongercjd <strongercjd@163.com>
2020-07-22 16:46:34 +0800

    上传CRC&RNG代码

---
+++ 10-CRC&RNG/main.c
@@ -0,0 +1,211 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F2xx_StdPeriph_Template/main.c 
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    13-April-2012
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "LCD_disp.h"
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static __IO uint32_t TimingDelay;
+
+#define BUFFER_SIZE    114
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static const uint32_t DataBuffer[BUFFER_SIZE] =
+  {
+    0x00001021, 0x20423063, 0x408450a5, 0x60c670e7, 0x9129a14a, 0xb16bc18c,
+    0xd1ade1ce, 0xf1ef1231, 0x32732252, 0x52b54294, 0x72f762d6, 0x93398318,
+    0xa35ad3bd, 0xc39cf3ff, 0xe3de2462, 0x34430420, 0x64e674c7, 0x44a45485,
+    0xa56ab54b, 0x85289509, 0xf5cfc5ac, 0xd58d3653, 0x26721611, 0x063076d7,
+    0x569546b4, 0xb75ba77a, 0x97198738, 0xf7dfe7fe, 0xc7bc48c4, 0x58e56886,
+    0x78a70840, 0x18612802, 0xc9ccd9ed, 0xe98ef9af, 0x89489969, 0xa90ab92b,
+    0x4ad47ab7, 0x6a961a71, 0x0a503a33, 0x2a12dbfd, 0xfbbfeb9e, 0x9b798b58,
+    0xbb3bab1a, 0x6ca67c87, 0x5cc52c22, 0x3c030c60, 0x1c41edae, 0xfd8fcdec,
+    0xad2abd0b, 0x8d689d49, 0x7e976eb6, 0x5ed54ef4, 0x2e321e51, 0x0e70ff9f,
+    0xefbedfdd, 0xcffcbf1b, 0x9f598f78, 0x918881a9, 0xb1caa1eb, 0xd10cc12d,
+    0xe16f1080, 0x00a130c2, 0x20e35004, 0x40257046, 0x83b99398, 0xa3fbb3da,
+    0xc33dd31c, 0xe37ff35e, 0x129022f3, 0x32d24235, 0x52146277, 0x7256b5ea,
+    0x95a88589, 0xf56ee54f, 0xd52cc50d, 0x34e224c3, 0x04817466, 0x64475424,
+    0x4405a7db, 0xb7fa8799, 0xe75ff77e, 0xc71dd73c, 0x26d336f2, 0x069116b0,
+    0x76764615, 0x5634d94c, 0xc96df90e, 0xe92f99c8, 0xb98aa9ab, 0x58444865,
+    0x78066827, 0x18c008e1, 0x28a3cb7d, 0xdb5ceb3f, 0xfb1e8bf9, 0x9bd8abbb,
+    0x4a755a54, 0x6a377a16, 0x0af11ad0, 0x2ab33a92, 0xed0fdd6c, 0xcd4dbdaa,
+    0xad8b9de8, 0x8dc97c26, 0x5c644c45, 0x3ca22c83, 0x1ce00cc1, 0xef1fff3e,
+    0xdf7caf9b, 0xbfba8fd9, 0x9ff86e17, 0x7e364e55, 0x2e933eb2, 0x0ed11ef0
+  };
+
+__IO uint32_t CRCValue = 0;
+
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  char str[80];
+	uint32_t random32bit = 0;
+  
+  RCC_ClocksTypeDef RCC_Clocks;
+  GPIO_InitTypeDef  GPIO_Init_s;
+  
+  /* 配置SysTick为1ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 1000);
+  
+  /* 使能GPIOE端口时钟 */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+  
+  /* 配置LED管脚 */
+  GPIO_Init_s.GPIO_Pin = GPIO_Pin_4;
+  GPIO_Init_s.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_Init_s.GPIO_OType = GPIO_OType_PP;
+  GPIO_Init_s.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init_s.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOE, &GPIO_Init_s);
+  
+
+  LCD_Init();
+  
+  /* Enable CRC clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_CRC, ENABLE);
+  
+  /* Compute the CRC of "DataBuffer" */
+  CRCValue = CRC_CalcBlockCRC((uint32_t *)DataBuffer, BUFFER_SIZE);
+  
+  sprintf(str, "CRC32 = 0X%X", CRCValue);
+  
+  LCD_ShowString(0,16*0,(char*)str);
+  
+  /* Enable RNG clock source */
+  RCC_AHB2PeriphClockCmd(RCC_AHB2Periph_RNG, ENABLE);
+  
+  /* RNG Peripheral enable */
+  RNG_Cmd(ENABLE);
+
+  while (1)
+  {
+    /* Wait until one RNG number is ready */
+    while(RNG_GetFlagStatus(RNG_FLAG_DRDY)== RESET)
+    {
+    }
+    
+    /* Get a 32bit Random number */       
+    random32bit = RNG_GetRandomNumber();
+    
+    sprintf(str, "RNG = 0X%X", random32bit);
+    
+    LCD_ShowString(0,16*1,(char*)str);
+    
+    GPIO_SetBits(GPIOE,GPIO_Pin_4);  //熄灭LED灯
+    Delay(500);                      //延时500ms
+    GPIO_ResetBits(GPIOE,GPIO_Pin_4);//点亮LED灯
+    Delay(500);  
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in 10 ms.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{
+  TimingDelay = nTime;
+
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+
+/*120Mhz时钟时，当ulCount为1时，函数耗时3个时钟，延时=3*1/120us=1/40us*/
+/*
+SystemCoreClock=120000000
+
+us级延时,延时n微秒
+SysCtlDelay(n*(SystemCoreClock/3000000));
+
+ms级延时,延时n毫秒
+SysCtlDelay(n*(SystemCoreClock/3000));
+
+m级延时,延时n秒
+SysCtlDelay(n*(SystemCoreClock/3));
+*/
+
+#if defined   (__CC_ARM) /*!< ARM Compiler */
+__asm void
+SysCtlDelay(unsigned long ulCount)
+{
+    subs    r0, #1;
+    bne     SysCtlDelay;
+    bx      lr;
+}
+#elif defined ( __ICCARM__ ) /*!< IAR Compiler */
+void
+SysCtlDelay(unsigned long ulCount)
+{
+    __asm("    subs    r0, #1\n"
+       "    bne.n   SysCtlDelay\n"
+       "    bx      lr");
+}
+
+#elif defined (__GNUC__) /*!< GNU Compiler */
+void __attribute__((naked))
+SysCtlDelay(unsigned long ulCount)
+{
+    __asm("    subs    r0, #1\n"
+       "    bne     SysCtlDelay\n"
+       "    bx      lr");
+}
+
+#elif defined  (__TASKING__) /*!< TASKING Compiler */                           
+/*无*/
+#endif /* __CC_ARM */
+
+/**
+  * @}
+  */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
